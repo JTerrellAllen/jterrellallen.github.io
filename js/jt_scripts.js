813f14686a025daa25710a92eade9e08e746fd26
JTerrellAllen <jterrellallen@gmail.com>
2014-04-12 17:40:16 -0500

    Add CSS animation and WOW

--- js/jt_scripts.js
+++ js/jt_scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(funciton() {
 
-  if( location.href == "http://jterrellallen.net" )
+  if( location.href == "http://jterrellallen.net/" || location.href == "http://www.jterrellallen.net/" )
   {
     $('#text-logo').addClass('animated fadeInLeft')
   }
-
-
 });
