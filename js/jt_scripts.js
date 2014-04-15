21ec65f0c811919a9d137f15d2d4b0bb8ebce584
JTerrellAllen <jterrellallen@gmail.com>
2014-04-15 18:00:35 -0500

    Update index

--- js/jt_scripts.js
+++ js/jt_scripts.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 
   if( location.href == "http://jterrellallen.net/" || location.href == "http://www.jterrellallen.net/" )
   {
-    $('#text-logo').addClass('animated fadeInLeft')
+    $('#text-logo').addClass('animated fadeInDown')
   }
 });
